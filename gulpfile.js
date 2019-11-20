46c488e98c9fec170bf99bf3aa340bd417f4b807
mambzero <mambzero@abv.bg>
2019-11-20 19:22:05 +0200

    Gulp updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let gulp = require('gulp'),
     sass = require('gulp-sass'),
     shell = require('gulp-shell');
 
-gulp.task('jekyll', shell.task(['bundle exec jekyll build --config _config.yml,_config_dev.yml']));
+gulp.task('jekyll', shell.task(['bundle exec jekyll build --config _config.yml']));
 
 gulp.task('sass', function() {
     return gulp
@@ -12,16 +12,20 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['./_assets/sass/styles.scss', './_sass/**/*.scss'], gulp.series('sass'));
+    gulp.watch([
+        './_assets/sass/styles.scss', 
+        './_sass/**/*.scss'
+        ], 
+        gulp.series('sass')
+    );
     gulp.watch([
         './_data/**/*.*', 
         './_includes/**/*.html', 
         './_layouts/**/*.html',
-        //'./_assets/**/*.*',
         './collections/**/*.html',
         './*.html'
         ], 
-        gulp.series('jekyll', 'sass')
+        gulp.series('jekyll')
     );
 });
 
