f3a6ddb5f75b23271e4111996b82df72c2c49d9f
mambzero <mambzero@abv.bg>
2019-11-21 20:08:47 +0200

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
     shell = require('gulp-shell'),
+    browserSync = require('browser-sync'),
     deploy = require('gulp-gh-pages');
+
+const root = '_site';
 
 gulp.task('jekyll-dev', shell.task(['bundle exec jekyll build --config _config.yml,_config_dev.yml']));
 gulp.task('jekyll-prod', shell.task(['bundle exec jekyll build --config _config.yml']));
@@ -10,10 +13,20 @@ gulp.task('sass', function() {
     return gulp
     .src(['./_assets/sass/styles.scss'])
     .pipe(sass({includePaths: ['./_sass/']}))
-    .pipe(gulp.dest('./_site/assets/css'));
+    .pipe(gulp.dest('./_site/assets/css'))
+    .pipe( browserSync.stream({ match: '**/*.css' }));
 });
 
 gulp.task('watch', function() {
+
+    browserSync.init({
+        files: [root + '/**'],
+        port: 4000,
+        server: {
+          baseDir: root
+        }
+    });
+
     gulp.watch([
         './_assets/sass/styles.scss',
         './_sass/**/*.scss'
@@ -30,7 +43,7 @@ gulp.task('watch', function() {
         gulp.series('jekyll-dev','sass')
     );
 });
-
+  
 gulp.task('default', gulp.series('jekyll-dev', 'sass', 'watch'));
 
 gulp.task('deploy-gh-pages', function() {
